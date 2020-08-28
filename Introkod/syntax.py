81d5828d44f70d40b8e865318fb93afddb4181eb
laullopes <70264630+laullopes@users.noreply.github.com>
2020-08-28 09:38:11 +0200

    intro.lektion 28/08

---
+++ Introkod/syntax.py
@@ -0,0 +1,12 @@
+print ("hej TE19C")
+
+# skapar en variabel och tilldelar det värdet "Lauanda"
+name = "Lauanda"
+
+print(f"Ditt namn är {name}")
+
+#float tyomvadlar strängen till ett flyttal för att kunna räkna med det
+side = float(input("ange kvadratens sida:"))
+area = side**2 
+
+print(f"arean av kvadraten är {area} a.e")
